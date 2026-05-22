e78aa015a163af142366d242a375e747b6560fc6
matien19 <matienkasa@gmail.com>
2026-05-22 11:33:54 +0700

    laravel-ui

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\AttendanceController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AttendanceController::class, 'index'])->name('home');
 Route::post('/verify-face', [AttendanceController::class, 'verify']);
 Route::post('/register-face', [AttendanceController::class, 'registerFace']); // Opsional untuk daftar
 Route::get('/ronaldo-detector', [AttendanceController::class, 'ronaldoDetector'])->name('ronaldo');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
